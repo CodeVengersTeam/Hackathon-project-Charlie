01e2d1d538225a20a3a0b148fe6c5dc9e25c8c87
Dheeraj Lalwani <lalwanidheeraj1234@gmail.com>
2020-07-03 13:49:42 +0530

    debug product page

--- product.php
+++ product.php
@@ -4,12 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="product.css">
-    <title>Product Inventory</title>
+    <title>product inventory</title>
 </head>
 <body>
 
 <?php
-if($_POST["Add to Inventory"] == 1)
+if($_POST["addproduct"] == 1)
 {
 
 ?>
@@ -21,19 +21,27 @@ if($_POST["Add to Inventory"] == 1)
         <input type="text" name="productname">
     </div>
     <div>
-        <label for="priceperpiece">Price per Piece</label>
-        <input type="text" name="priceperpiece">
+        <label for="productimage">Product Image</label>
+        <input type="text" name="productimage">
     </div>
     <div>
-        <label for="numberofpieces">Number of Pieces</label>
-        <input type="text" name="numberofpieces">
+        <label for="priceperunit">Price per Piece</label>
+        <input type="text" name="priceperunit">
     </div>
     <div>
-        <label for="manufcturingdate">Manufacturing Date</label>
-        <input type="date" name="manufcturingdate">
+        <label for="inventoryquantity">Number of Pieces</label>
+        <input type="text" name="inventoryquantity">
     </div>
     <div>
-        
+        <label for="manufacturingdate">Manufacturing Date</label>
+        <input type="date" name="manufacturingdate">
+    </div>
+    <div>
+        <label for="tags">Tags</label>
+        <input type="text" name="tags">
+    </div>
+    <div>
+        <input type="hidden" name="addproduct" value=1>
         <input type="submit"  value="Add to Inventory">
     </div>
    
