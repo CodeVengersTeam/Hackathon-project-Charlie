3e020359dc374e8cf839121ad4ce12b31dbd9192
mihikagaonkar <mihika831@gmail.com>
2020-07-04 02:42:24 +0530

    make customerfeedback page

---
+++ customerfeedback.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="customerfeedback.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container">
+        <div class="name">
+            <label for="name">Name:</label>
+            <input type="text" name="name">
+        </div>
+        <div class="email">
+            <label for="email">Email:</label>
+            <input type="email" name="email">
+        </div>
+        <div class="phone">
+            <label for="phone">Phone Number:</label>
+            <input type="text" name="phone">
+        </div>
+        <div class="id"> 
+            <label for="orderid">Order id:</label>
+            <input type="text" name="orderid">
+        </div>
+        <div class="description">
+            <label for="description">Description:</label>
+            <input class="descriptioninput"type="text" name="description">
+        </div>
+        <div class="button"><input class="logininput" type="submit" value="Submit"></div>
+    </div>
+</body>
+</html>
