8116abefca1aceb7e2252fcd01806abe9d47df29
mihikagaonkar <mihika831@gmail.com>
2020-07-03 19:51:24 +0530

    rename files

---
+++ customerindex.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="customerindex.css" type="text/css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container">
+        <div class="tile1">
+            <h1>Buy</h1>
+        </div>
+        <div class="tile2">
+            <h1>View Orders</h1>
+        </div>
+        <div class="tile3">
+            <h1>Cart</h1>
+        </div>
+    </div>
+</body>
+</html>
