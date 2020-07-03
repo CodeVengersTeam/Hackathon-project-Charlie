022faccb3c6db729ecfe040b56ab5b69291c1a25
mihikagaonkar <mihika831@gmail.com>
2020-07-03 21:48:16 +0530

    changed file name to customerlogin

---
+++ landing.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="landing.css">
+    <title>home page/landing page</title>
+</head>
+<body>
+    <div class="container">
+        <a href="adminlogin.php" class="tile1">
+            <h1>Admin</h1>
+        </a>
+        <a href="customerlogin.php" class="tile2">
+            <h1>Customer</h1>
+        </a>       
+    </div>
+    
+</body>
+</html>
+
