4f1bd875283223fe373b861abf1195345b02320f
Dheeraj Lalwani <lalwanidheeraj1234@gmail.com>
2020-07-05 09:18:28 +0530

    links linking

--- customerindex.php
+++ customerindex.php
@@ -14,15 +14,12 @@ if ($_POST["loginsuccess"] == 1)
 
 ?>
     <div class="container">
-        <div class="tile1">
-            <h1>Buy</h1>
-        </div>
+    <a class="tile1" href="customerviewproduct.php">Buy</a>
         <div class="tile2">
             <h1>View Orders</h1>
         </div>
-        <div class="tile3">
-            <h1>Cart</h1>
-        </div>
+        <a class="tile3" href="cart.php">Cart</a>
+        <a class="tile4" href="feedback.php">Customer Feedback</a>
     </div>
 
 <?php
