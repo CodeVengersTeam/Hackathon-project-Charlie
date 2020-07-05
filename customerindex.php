1ae93e46ff4096b9f8fe16b6bb9b1d8bb45e5c12
Dheeraj Lalwani <lalwanidheeraj1234@gmail.com>
2020-07-05 09:37:11 +0530

    fix font size

--- customerindex.php
+++ customerindex.php
@@ -14,12 +14,12 @@ if ($_POST["loginsuccess"] == 1)
 
 ?>
     <div class="container">
-    <a class="tile1" href="customerviewproduct.php">Buy</a>
+    <a class="tile1" href="customerviewproduct.php"> <h1>Buy</h1></a>
         <div class="tile2">
             <h1>View Orders</h1>
         </div>
-        <a class="tile3" href="cart.php">Cart</a>
-        <a class="tile4" href="feedback.php">Customer Feedback</a>
+        <a class="tile3" href="cart.php"> <h1>Cart</h1></a>
+        <a class="tile4" href="feedback.php"> <h1>Customer Feedback</h1></a>
     </div>
 
 <?php
